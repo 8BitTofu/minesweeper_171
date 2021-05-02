083ec119ac5f1dfd2889ad479b9c42cfb5ed1e9c
Austin Leung <atleung1@uci.edu>
2021-05-02 07:00:46 -0700

    semi-working

--- src/MyAI.py
+++ src/MyAI.py
@@ -18,6 +18,9 @@ from Action import Action
 import numpy as np
 import queue
 
+# austin's shortcut notes:
+# python src/Main.py -d -f ../WorldGenerator/Problems/
+# cd Documents/uci/Spring 2021/CS171/minesweeper/Minesweeper_Student-master/Minesweeper_Python
 
 
 class MyAI( AI ):
@@ -34,9 +37,12 @@ class MyAI( AI ):
 		self.colDimension = colDimension # x-value
 
 		self.coveredTiles:int = rowDimension*colDimension
+		self.numFlaggedTiles:int = 0
 		self.totalTimeElapsed:float = 0.0
 		self.totalMines:int = totalMines
 		
+		# print("row dimension: ", rowDimension)
+		# print("col dimension: ", colDimension)
 
 		# Instantiate action queue
 		self.actionQueue = queue.Queue()
@@ -64,38 +70,95 @@ class MyAI( AI ):
 		########################################################################
 
 		# Check if goal reached
-		if (self.coveredTiles == self.totalMines):
+		if (self.numFlaggedTiles == self.totalMines):
+			# print("coveredTiles == totalMines -> LEAVE")
 			return Action(AI.Action.LEAVE)
 
 		else:
 			# Specific Tile Definitions
-			numFlaggedNeighbors = -1
-			numCoveredNeighbors = 8
-			tileLabel = -1 ## tile number (i.e. 0-8)
+			numFlaggedNeighbors:int = 0
+			numCoveredNeighbors:int = 8
+			singleUncoveredTileX:int = -1
+			singleUncoveredTileY:int = -1
 			# effectiveLabel = tileLabel - numFlaggedNeighbors
 
 			if (number != -1):
-				prevMoveX = self.currentAction.getX()
-				prevMoveY = self.currentAction.getY()
-				prevMoveAction = self.currentAction.getMove()
+				# print("number != -1")
+				prevMoveX:int = self.currentAction.getX()
+				prevMoveY:int = self.currentAction.getY()
+				prevMoveAction:str = self.currentAction.getMove()
 
-				# basic case: 0 tile
-				if (prevMoveAction == "UNCOVER"):
+				# print("prevMoveAction:", prevMoveAction)
+				# print("prevMoveX:", prevMoveX)
+				# print("prevMoveY:", prevMoveY)
+
+				if (prevMoveAction == AI.Action.UNCOVER):
+					# add previous tile to uncovered
 					self.uncoveredTiles.add((prevMoveX, prevMoveY))
+					
+					# update board to previous tile
+					self.msBoard.updateBoard(prevMoveX, prevMoveY, number)
+					self.msBoard.printBoard()
 					self.coveredTiles -= 1
 
+					# basic case: 0 tile
 					if (number == 0):
 						# check surrounding neighbors to uncover if covered
 						for i in range(-1,2):
 							for j in range(-1,2):
-								if (self.validBounds(prevMoveX + i, prevMoveY + j)) and
-								(prevMoveX + i, prevMoveY + j) not in self.uncoveredTiles:
-										self.actionQueue.put(Action(AI.Action.UNCOVER, prevMoveX + i, prevMoveY + j))
+								#print("checking: ", i, j)
+								#print(prevMoveX + i, prevMoveY + j)
+								#print("validbounds: ", self.validBounds(prevMoveX + i, prevMoveY + j))
+								#print("checkboards: ", self.msBoard.checkBoard(prevMoveX + i, prevMoveY + j) == -1, self.msBoard.checkBoard(prevMoveX + i, prevMoveY + j))
+								#self.msBoard.printBoard()
+								if (self.validBounds(prevMoveX + i, prevMoveY + j)) and self.msBoard.checkBoard(prevMoveX + i, prevMoveY + j) == -1:
+									# print("ADDEDQUEUE for uncover")
+									self.actionQueue.put(Action(AI.Action.UNCOVER, prevMoveX + i, prevMoveY + j))
+			
+
+					# basic case: 1 tile
+					if (number == 1):
+						# check surrounding neighbors to see whats covered
+						# if only one tile is uncovered -> flag it
+						# else, ignore this tile and continue (basically go around this 1 tile and continue until complete)
+						for i in range(-1,2):
+							for j in range(-1,2):
+								if (self.validBounds(prevMoveX + i, prevMoveY + j)) == False or self.msBoard.checkBoard(prevMoveX + i, prevMoveY + j) != -1:
+									numCoveredNeighbors -= 1
+
+								else:
+									singleUncoveredTileX = prevMoveX + i 
+									singleUncoveredTileY = prevMoveY + j
+
+						# if only one covered tile -> flag
+						if (numCoveredNeighbors == 1):
+							self.actionQueue.put(Action(AI.Action.FLAG, singleUncoveredTileX, singleUncoveredTileY))
+				
+
+				# Flag is hardcoded for SuperEasy Worlds #####################################
+				if (prevMoveAction == AI.Action.FLAG):
+					self.msBoard.updateBoard(prevMoveX, prevMoveY, number)
+					self.numFlaggedTiles += 1
+
+					for i in range(-1,2):
+						for j in range(-1,2):
+							if (self.validBounds(prevMoveX + i, prevMoveY + j)) and (prevMoveX + i, prevMoveY + j) not in self.uncoveredTiles:
+								self.actionQueue.put(Action(AI.Action.UNCOVER, prevMoveX + i, prevMoveY + j))
+
+
+
+			# get latest current action and
+			self.currentAction = self.actionQueue.get()
+			# print("NEXT ACTION (move, x, y): ", self.currentAction.getMove(), self.currentAction.getX(), self.currentAction.getY())
+			# for q_item in self.actionQueue.queue:
+			# 	print(q_item.getMove(), q_item.getX(), q_item.getY())
+
+			return self.currentAction
 
 	
 	def validBounds(self, xvalue: int, yvalue: int) -> bool:
 		''' return boolean to check if tile is within board boundaries'''
-		if (xvalue <= self.colDimension) and (xvalue > 0) and (yvalue <= self.rowDimension) and (yvalue > 0):
+		if (xvalue < self.colDimension) and (xvalue >= 0) and (yvalue < self.rowDimension) and (yvalue >= 0):
 			return True
 		else:
 			return False
@@ -157,7 +220,7 @@ class Board:
 		self.uncoveredTileRange = set(range(-2, 8))
  
 		# create fully covered board
-		self.board = np.full((colDimension, rowDimension), fill_value = -1)
+		self.board = np.full((colDimension+1, rowDimension+1), fill_value = -1)
 		self.totalMines = totalMines
 
 		self.board[startX, startY] = 0
@@ -165,6 +228,17 @@ class Board:
 
 	def updateBoard(self, positionX, positionY, tileValue):
 		''' pass in action at certain position and update board '''
+		# print("updated ", positionX, positionY, tileValue)
+		self.board[positionX, positionY] = tileValue
+		# print(self.board)
+
+	def checkBoard(self, positionX, positionY) -> int:
+		''' return tileValue at position '''
+		return self.board[positionX, positionY]
+	
+	def printBoard(self):
+		''' debugging purposes - print full board '''
+		#print(self.board)
 		pass
 		
 
