c546af089df354168f5b8c60441e1b9e93e2ab0a
Austin Leung <atleung1@uci.edu>
2021-06-03 02:18:15 -0700

    fixed minor error / new state

--- src/MyAI.py
+++ src/MyAI.py
@@ -15,8 +15,7 @@
 from AI import AI
 from Action import Action
 
-# import numpy as np
-import queue
+import random
 import collections
 
 
@@ -43,8 +42,7 @@ class MyAI(AI):
         self.totalTimeElapsed: float = 0.0
         self.totalMines: int = totalMines
 
-        # print("row dimension: ", rowDimension)
-        # print("col dimension: ", colDimension)
+
 
         # Instantiate action queue
         self.actionQueue = collections.deque()
@@ -56,7 +54,7 @@ class MyAI(AI):
         self.flaggedSpots = set()
         for i in range(-1, 2):
             for j in range(-1, 2):
-                if self.validBounds(startX + i, startY + j) and self.msBoard.checkBoard(startX + i, startY + j) == -1 and (startX + i, startY + j) not in self.uncoveredTiles and (startX + i, startY + j) not in self.currentQueue:
+                if self.validBounds(startX + i, startY + j):
                     self.actionQueue.append(Action(AI.Action.UNCOVER, startX + i, startY + j))
                     self.currentQueue.add((startX + i, startY + j))
         self.flaggedEffectiveCount = dict()
@@ -80,8 +78,10 @@ class MyAI(AI):
 
         # Check if goal reached
 
-
         self.msBoard.printBoard()
+
+
+        #self.msBoard.printBoard()
         if len(self.uncoveredTiles) == (self.colDimension * self.rowDimension) - 1:
             return Action(AI.Action.LEAVE, 1, 1)
 
@@ -94,81 +94,94 @@ class MyAI(AI):
             #singleUncoveredTileY: int = -1
             # effectiveLabel = tileLabel - numFlaggedNeighbors
 
-            #update effective label
-            #print("uncovered tiles: ",self.uncoveredTiles)
-            if len(self.flaggedSpots) != 0:
+            # self.msBoard.printBoard()
 
-
-                for flags in self.flaggedSpots:
-                    for i in range(-1, 2):
-                        for j in range(-1, 2):
-
-                            if self.validBounds(flags[0] + i,flags[1] + j) and (flags[0] + i, flags[1] + j) not in self.flaggedEffectiveCount[(flags[0], flags[1])] and self.msBoard.checkBoard(flags[0] + i,flags[1] + j)  > -1:
-                                self.flaggedEffectiveCount[(flags[0], flags[1])].add((flags[0] + i, flags[1] + j))
-                                current_value = self.msBoard.checkBoard(flags[0] + i, flags[1] + j)
-                                self.msBoard.updateBoard(flags[0] + i, flags[1] + j, current_value - 1)
-
-                                if current_value == 1:
-                                    _x = flags[0] + i
-                                    _y = flags[1] + j
-                                    for k in range(-1, 2):
-                                        for l in range(-1, 2):
-                                            if k == 0 and l == 0:
-                                                continue
-                                            if self.validBounds(_x + k, _y + l) and self.msBoard.checkBoard(_x + k, _y + l) == -1 and (_x + k, _y + l) not in self.currentQueue and (_x + k, _y + l) not in self.flaggedEffectiveCount[(flags[0], flags[1])] and (_x + k, _y + l) not in self.uncoveredTiles and (_x + k, _y + l) != (self.currentAction.getX(), self.currentAction.getY()):
-                                                self.currentQueue.add((_x + k, _y + l))
-                                                self.actionQueue.append(Action(AI.Action.UNCOVER,_x + k, _y + l))
-
-
+            #print("FLAGS", self.flag_checker())
             prevMoveX: int = self.currentAction.getX()
             prevMoveY: int = self.currentAction.getY()
             prevMoveAction = self.currentAction.getMove()
+            self.msBoard.updateBoard(prevMoveX, prevMoveY, number)
+            self.coveredTiles -= 1
+
+            
+
+            if len(self.flaggedSpots) != 0:
+                # update effective label
+                # print("updated flags")
+                self.update_effective_number()
 
 
             if (prevMoveAction == AI.Action.UNCOVER):
 
-
                 # update board to previous tile
+                length_of_queue =  len(self.currentQueue)
+                """
                 self.msBoard.updateBoard(prevMoveX, prevMoveY, number)
                 self.coveredTiles -= 1
+                """
 
-                for i in range(-1, 2):
-                    for j in range(-1, 2):
-                        if (self.validBounds(prevMoveX + i, prevMoveY + j)) and self.msBoard.checkBoard(prevMoveX + i, prevMoveY + j) == -1 and ((prevMoveX + i, prevMoveY + j) not in self.uncoveredTiles and (prevMoveX + i, prevMoveY + j) not in self.currentQueue) and self.msBoard.checkBoard(prevMoveX,prevMoveY) == 0:
-                            self.actionQueue.append(Action(AI.Action.UNCOVER, prevMoveX + i, prevMoveY + j))
-                            self.currentQueue.add((prevMoveX + i, prevMoveY + j))
-
-
-                    # check surrounding neighbors to see whats covered
-                    # if only one tile is uncovered -> flag it
-                    # else, ignore this tile and continue (basically go around this 1 tile and continue until complete)
+                self.add_safe_spots(prevMoveX,prevMoveY)
                 new_corners = self.cornerChecker()
-
+                """ 
                 if len(new_corners) != 0:
                     for element in new_corners:
                         if (element[0],element[1]) not in self.currentQueue and (element[0],element[1]) not in self.uncoveredTiles:
                             self.actionQueue.appendleft(Action(AI.Action.FLAG,element[0],element[1]))
-                            self.currentQueue.add((element[0], element[1]))
+                            self.currentQueue.add((element[0], element[1]))                    
+                        
 
-            # Flag is hardcoded for SuperEasy Worlds #####################################
+
+                """
+
+                #if length_of_queue == len(self.currentQueue):
+                    #self.flag_checker()
+                #print("new_corners",new_corners)
+
             if (prevMoveAction == AI.Action.FLAG):
                 self.coveredTiles -= 1
-
                 self.numFlaggedTiles += 1
+                self.msBoard.updateBoard(self.currentAction.getX(), self.currentAction.getY(), -2) # flag the board
+
+
+            # if action queue is empty do these
+            if len(self.actionQueue) == 0:
+                # print("NO MORE QUEUE")                
+                flags:list = self.flag_checker()
+
+
+            # if previous method didnt work -> try this
+            # last resort -> pick a random covered tile and uncover
+            if len(self.actionQueue) == 0:
+                # print("LAST RESORT")
+                tile:tuple = self.msBoard.getRandomCoveredTile()
+                self.actionQueue.append(Action(AI.Action.UNCOVER, tile[0], tile[1]))
+                self.currentQueue.add((tile[0], tile[1]))
+
+
+            frontier_list = self.frontier()
+            print("Frontier List",frontier_list)
+            print("Queue",self.currentQueue)
+            print("actionQueue: ")
+            for action in self.actionQueue:
+                print(str(action.getMove()) + " at (" + str(action.getX()) + "," + str(action.getY()) + ")")
 
             self.currentAction = self.actionQueue.popleft()
+
+            # print("remove: ", self.currentAction.getX(), self.currentAction.getY())
+
             self.currentQueue.remove((self.currentAction.getX(), self.currentAction.getY()))
             self.uncoveredTiles.add((prevMoveX, prevMoveY))
 
+            print("next action: " + str(self.currentAction.getMove()) + " at (" + str(self.currentAction.getX()) + "," + str(self.currentAction.getY()) + ")")
             return self.currentAction
+                
+
 
     def validBounds(self, xvalue: int, yvalue: int) -> bool:
         ''' return boolean to check if tile is within board boundaries'''
         if xvalue in range(0, self.colDimension) and yvalue in range(0, self.rowDimension):
             return True
         else:
-            # print("col dimension: ", self.colDimension, " xvalue: ", xvalue)
-            # print("row dimension: ", self.rowDimension, " yvalue: ", yvalue)
             return False
 
     # RULE OF THUMBS NOTES
@@ -214,11 +227,11 @@ class MyAI(AI):
                             continue
                         if (self.validBounds(_x + i, _y + j)) == False:
                             numCoveredNeighbors -= 1
-                        elif (self.msBoard.checkBoard(_x + i, _y + j) > -1 and self.msBoard.checkBoard(_x , _y) > -1):
+                        elif ((self.msBoard.checkBoard(_x + i, _y + j) > -1 or self.msBoard.checkBoard(_x + i, _y + j) == -2) and self.msBoard.checkBoard(_x , _y) > -1):
                             numCoveredNeighbors -= 1
                 if self.msBoard.checkBoard(_x, _y) > 0 and numCoveredNeighbors == self.msBoard.checkBoard(_x,_y):
                     new_corners.append((_x, _y))
-
+        #print("new corners inside func",new_corners)
         return_list = []
         for element in new_corners:
             for i in range(-1, 2):
@@ -233,6 +246,130 @@ class MyAI(AI):
                         self.actionQueue.appendleft(Action(AI.Action.FLAG, element[0] + i, element[1] + j))
                         self.currentQueue.add((element[0] + i, element[1] + j))
         return return_list
+
+    def add_safe_spots(self, prevMoveX, prevMoveY):
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if (self.validBounds(prevMoveX + i, prevMoveY + j)) and self.msBoard.checkBoard(prevMoveX + i,
+                                                                                                prevMoveY + j) == -1 \
+                        and ((prevMoveX + i, prevMoveY + j) not in self.uncoveredTiles and (
+                prevMoveX + i, prevMoveY + j) not in self.currentQueue) \
+                        and self.msBoard.checkBoard(prevMoveX, prevMoveY) == 0:
+                    self.actionQueue.append(Action(AI.Action.UNCOVER, prevMoveX + i, prevMoveY + j))
+                    self.currentQueue.add((prevMoveX + i, prevMoveY + j))
+
+
+    def update_effective_number(self):
+        for flags in self.flaggedSpots:
+            for i in range(-1, 2):
+                for j in range(-1, 2):
+                    if self.validBounds(flags[0] + i, flags[1] + j) and (flags[0] + i, flags[1] + j) not in \
+                            self.flaggedEffectiveCount[(flags[0], flags[1])] \
+                            and self.msBoard.checkBoard(flags[0] + i, flags[1] + j) > -1:
+                        self.flaggedEffectiveCount[(flags[0], flags[1])].add((flags[0] + i, flags[1] + j))
+                        current_value = self.msBoard.checkBoard(flags[0] + i, flags[1] + j)
+                        self.msBoard.updateBoard(flags[0] + i, flags[1] + j, current_value - 1)
+
+                        if current_value == 1:
+                            _x = flags[0] + i
+                            _y = flags[1] + j
+                            for k in range(-1, 2):
+                                for l in range(-1, 2):
+                                    if k == 0 and l == 0:
+                                        continue
+                                    if self.validBounds(_x + k, _y + l) and self.msBoard.checkBoard(_x + k,
+                                                                                                    _y + l) == -1 \
+                                            and (_x + k, _y + l) not in self.currentQueue and (_x + k, _y + l) not in \
+                                            self.flaggedEffectiveCount[(flags[0], flags[1])] \
+                                            and (_x + k, _y + l) not in self.uncoveredTiles and (_x + k, _y + l) != (
+                                    self.currentAction.getX(), self.currentAction.getY()):
+                                        self.currentQueue.add((_x + k, _y + l))
+                                        self.actionQueue.append(Action(AI.Action.UNCOVER, _x + k, _y + l))
+
+    def frontier(self):
+        frontier_list = list()
+        for _x in range(self.rowDimension):
+            for _y in range(self.colDimension):
+                #Check if only one side is unknown Only checking right, left, up down
+                list_of_adjacent = list()
+                number_of_valid_sides = 0
+
+                for i in range(-1,2):
+                    for j in range(-1,2):
+                        if i == 0 and j == 0:
+                            continue
+                        elif self.validBounds(_x + i, _y + j):
+                            # if _x == 0 and _y == 1:
+                                # print("(" + str(_x + i) + "," + str(_y + j) + "): " + str(self.msBoard.checkBoard(_x + i, _y + j)))
+                            list_of_adjacent.append(self.msBoard.checkBoard(_x + i, _y + j))
+                            number_of_valid_sides += 1
+                
+                
+                # if self.validBounds(_x - 1, _y):
+                #     list_of_adjacent.append(self.msBoard.checkBoard(_x - 1, _y))
+                #     number_of_valid_sides += 1
+                # if self.validBounds(_x + 1, _y):
+                #     list_of_adjacent.append(self.msBoard.checkBoard(_x + 1, _y))
+                #     number_of_valid_sides += 1
+                # if self.validBounds(_x, _y - 1):
+                #     list_of_adjacent.append(self.msBoard.checkBoard(_x, _y - 1))
+                #     number_of_valid_sides += 1
+                # if self.validBounds(_x, _y + 1):
+                #     list_of_adjacent.append(self.msBoard.checkBoard(_x, _y + 1))
+                #     number_of_valid_sides += 1
+
+                number_of_unknown = 0
+                for element in list_of_adjacent:
+                    if element == -1:
+                        number_of_unknown += 1
+                
+                # if _x == 0 and _y == 1:
+                    # print("number of unknown: ", number_of_unknown)
+
+                if number_of_unknown >= 1 and number_of_unknown < number_of_valid_sides and self.msBoard.checkBoard(_x,_y) != -1:
+                    frontier_list.append((_x, _y))
+        return frontier_list
+
+
+
+    def flag_checker(self):
+        new_flags = []
+        for _x in range(self.colDimension):
+            for _y in range(self.rowDimension):
+                numCoveredNeighbors = 8
+                for i in range(-1, 2):
+                    for j in range(-1, 2):
+                        if i == 0 and j == 0:
+                            continue
+                        if (self.validBounds(_x + i, _y + j)) == False:
+                            numCoveredNeighbors -= 1
+                        elif ((self.msBoard.checkBoard(_x + i, _y + j) > -1 or self.msBoard.checkBoard(_x + i, _y + j) == -2) and self.msBoard.checkBoard(_x, _y) > -1):
+                            numCoveredNeighbors -= 1
+                if self.msBoard.checkBoard(_x, _y) > 0 and numCoveredNeighbors == self.msBoard.checkBoard(_x, _y):
+                    new_flags.append((_x, _y))
+                """
+                if _x == 4 and _y == 4:
+                    print("x == 4, y == 4"," value ",self.msBoard.checkBoard(_x,_y),"numCovered",numCoveredNeighbors)
+                """
+        for _x, _y in new_flags:
+            for i in range(-1,2):
+                for j in range(-1, 2):
+                    if i == 0 and j == 0:
+                        continue
+                    if self.msBoard.checkBoard(_x + i,_y + j) == -1:
+                        self.flaggedSpots.add((_x + i, _y + j))
+                        self.msBoard.updateBoard(_x + i, _y + j, -2)
+                        self.flaggedEffectiveCount[(_x + i, _y + j)] = set()
+                        self.actionQueue.append(Action(AI.Action.FLAG, _x + i, _y + j))
+                        self.currentQueue.add((_x + i, _y + j))
+
+
+        return new_flags
+
+
+
+
+
 
 
 class Board:
@@ -250,8 +387,15 @@ class Board:
         self.tileStates = set(range(-2, 8))
 
         # create fully covered board
-        self.board = [[-1 for index in range(colDimension)] for index in range(rowDimension)]
+        self.board = [[-1 for index in range(rowDimension)] for index in range(colDimension)]
         self.totalMines = totalMines
+        self.colDimension = colDimension
+        self.rowDimension = rowDimension
+
+        # in theory: 
+        # row is y / # of rows = rowDimension
+        # col is x / # of cols = colDimension
+
 
         self.board[startX][startY] = 0
 
@@ -272,5 +416,18 @@ class Board:
             for _y in range(len(self.board[0])):
                 print(" ", self.board[_x][_y], " ", end='')
             print()
+
+    def getRandomCoveredTile(self) -> tuple:
+        ''' return coordinates of a random covered tile '''
+        tileCovered = False
+        
+        while tileCovered == False:
+            randomX = random.randint(0, self.colDimension - 1)
+            randomY = random.randint(0, self.rowDimension - 1)
+
+            if self.checkBoard(randomX, randomY) == -1:
+                return (randomX, randomY)
+
+
 
 
